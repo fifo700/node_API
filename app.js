16b863f08b8c35f342ffab98981e9ad1cdf4215b
fifo700 <farisbibo@gmail.com>
2024-10-23 16:22:40 +0200

    favicon

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express') 
+const morgan = require('morgan')
 const {success} = require('./helper.js')
 let pokemons = require('./mock-pokemon');
 
 const app = express()
 const port =  3000
 
-app.use ( (req, res, next) => {
-    console.log(`URL : ${req.url}`)
-    next()
-})
+app.use ( morgan('dev'))
 
 
 app.get('/', (req,res)=> res.send('Hello express 2'))
