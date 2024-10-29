e38fde65b3296921c312ffc76d1536fd8d0d98b2
fifo700 <farisbibo@gmail.com>
2024-10-29 17:37:44 +0100

    requete avancé(unique marche toujour pas

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,8 +1,29 @@
 const { Pokemon } = require('../db/sequelize')
-  
+const pokemon = require('../models/pokemon')
+const { Op} = require('sequelize')
+   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+
+    if(req.query.name){
+      const name=req.query.name
+      Pokemon.findAll({
+        where:{ 
+          name:{//name est la propriété du modèle pokemon
+            [Op.like]: `%${name}%`//name est le crtitère de recherche
+          }
+          } ,
+          limit: 5
+        })
+
+        .then(pokemons => {
+          const message =`il y a ${pokemons.length} pokémons qui corresponde a la recherche ${name}`
+          res.json({message,data: pokemons})
+        })
+    
+  }
+    else{
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
@@ -11,5 +32,5 @@ module.exports = (app) => {
       const message = "pas trouver les pokemons"
       res.status(500).json({message, data : error})
       })
-  })
+    }})
 }
--- src/routes/updatePokemons.js
+++ src/routes/updatePokemons.js
@@ -4,7 +4,7 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
   app.put('/api/pokemons/:id', (req, res) => {
     const id = req.params.id
-    Pokemon.update(req.body, {
+    Pokemon.updae(req.body, {
       where: { id: id }
     })
     .then(_ => {
@@ -24,8 +24,12 @@ module.exports = (app) => {
       if(error instanceof ValidationError){
         return res.status(400).json({ message:error.message, data : error})
       }
+      if(error instanceof UniqueConstraintError){
+        return res.status(400).json({ message:error.message, data : error})
+      }
       const message = `la modification n'a pas pu aboutir`
       res.status(500).json({message, data: error})
-    })
+    }
+    )
   })
 }
