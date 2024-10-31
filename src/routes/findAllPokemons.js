66f851d3a7e0a8cf810827d75aca5fa9205c9459
fifo700 <farisbibo@gmail.com>
2024-10-31 15:01:41 +0100

    limit fixer

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -3,10 +3,10 @@ const { Op} = require('sequelize')
    
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
+    const limit = parseInt(req.query.limit) || 5
 
     if(req.query.name){
       const name=req.query.name
-      const limit = parseInt(req.query.limit) || 5
       Pokemon.findAndCountAll({
         where:{ 
           name:{//name est la propriété du modèle pokemon
@@ -24,9 +24,12 @@ module.exports = (app) => {
     
   }
     else{
-      Pokemon.findAll({order:['name']})
+      Pokemon.findAll({order:['name'],
+        limit:limit
+      })
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
+
         res.json({ message, data: pokemons })
       })
       .catch(error => {
