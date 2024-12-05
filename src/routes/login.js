6f4fa9403f93178480fdc0e2f2555bd0c2ad48ee
fifo700 <farisbibo@gmail.com>
2024-12-05 15:57:29 +0100

    j'installe mysql12

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,6 @@
 /* Authentification : Créer un modèle User avec Sequelize */
 const { User } = require('../db/sequelize')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const privateKey = require('../auth/private_key')
 
